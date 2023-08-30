076431354db966f18b01a7731c457da8e99002e3
Warlei <warleihc21@gmail.com>
2023-08-29 22:19:27 -0300

    primeiro commit

---
+++ .phan/config.php
@@ -0,0 +1,32 @@
+<?php
+
+
+return [
+
+
+    "target_php_version" => 8.2,
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle',
+        'vendor/psr/http-message'
+    ],
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+    'plugins' => [
+        'AlwaysReturnPlugin',
+        'DollarDollarPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'PregRegexCheckerPlugin',
+        'PrintfCheckerPlugin',
+        'SleepCheckerPlugin',
+        // Checks for syntactically unreachable statements in
+        // the global scope or function bodies.
+        'UnreachableCodePlugin',
+        'UseReturnValuePlugin',
+        'EmptyStatementListPlugin',
+        'LoopVariableReusePlugin',
+    ],
+];
